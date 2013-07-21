85f2154ff1e3fff364a34a240240eff6a98a7580
Bryce Baril <bryce@ravenwall.com>
2013-07-21 12:56:40 -0700

    Converting to new stream-spigot interface

--- test/index.js
+++ test/index.js
@@ -24,7 +24,7 @@ test("no max (passthrough)", function (t) {
     t.equals(m.bytes, 8)
   }
 
-  spigot({content: content}).pipe(m).pipe(concat(match))
+  spigot([content]).pipe(m).pipe(concat(match))
 })
 
 test("under max", function (t) {
@@ -39,14 +39,13 @@ test("under max", function (t) {
     t.equals(m.bytes, 8)
   }
 
-  spigot({content: content}).pipe(m).pipe(concat(match))
+  spigot([content]).pipe(m).pipe(concat(match))
 })
 
 test("stops at meter", function (t) {
-  t.plan(4)
+  t.plan(3)
 
   var chunks = 0
-  var content = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
   function match(d) {
     t.fail()
@@ -57,20 +56,10 @@ test("stops at meter", function (t) {
   var m = meter(10)
   m.on("error", function (e) {
     t.ok(e.message, "Stream exceeded specified max of 10 bytes.")
-    t.equals(chunks, 4)
     // 12 because read frame is 4, so the 3rd read will put it over the max at 12 bytes
-    t.equals(c.getBody().toString(), content.substring(0, 12))
+    t.equals(c.getBody().toString(), "ABCDEFGHIJKL")
     t.equals(m.bytes, 12)
   })
 
-  var s = spigot({
-    content: content,
-    chunkSize: 4,
-  })
-  s.on("progress", function (s) {
-    chunks++
-  })
-
-
-  s.pipe(m).pipe(c)
+  spigot(["ABCD", "EFGH", "IJKL", "MNOP", "QRST", "UVWX", "YZ"]).pipe(m).pipe(c)
 })
